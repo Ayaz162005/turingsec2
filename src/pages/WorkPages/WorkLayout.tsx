589d44d6f706ba25b4997ab4e3f928e35ad34681
Ayaz <ayazumman@gmail.com>
2024-02-16 12:43:37 +0400

    sds

--- src/pages/WorkPages/WorkLayout.tsx
+++ src/pages/WorkPages/WorkLayout.tsx
@@ -2,18 +2,33 @@ import { Outlet, useNavigate } from "react-router";
 import Navbar from "../../components/shared/WorkerShared/Navbar";
 import { useCurrentUser } from "../../context/CurrentUser";
 import { useEffect, useState } from "react"; // Import useState
-import CompanyNavbar from "../../components/shared/Company/CompanyNavbar";
-import { set } from "date-fns";
+
+import SpinnerLoader from "../../components/component/SpinnerLoader";
 
 export default function WorkLayout() {
+  const [load, setLoad] = useState(false);
+  setTimeout(() => {
+    setLoad(true);
+  }, 500);
   const navigate = useNavigate();
   const { currentUser } = useCurrentUser();
-  const [loading, setLoading] = useState(false); // Add loading state
-  const arr: boolean[] = [];
-  console.log(currentUser);
+  // Add loading state
+  useEffect(() => {
+    console.log(currentUser, load);
+    if (!currentUser?.activated && load) {
+      navigate("/");
+      // setLoad(true);
+    }
+  }, [currentUser, load]);
 
   // Render the component only if currentUser is defined and activated
-
+  if (!load) {
+    return (
+      <div className="flex items-center justify-center bg-[#1E1E1E] h-screen">
+        <SpinnerLoader />
+      </div>
+    );
+  }
   return (
     <div className="font-sans relative md:ml-[270px] ml-[74px] flex">
       <Navbar />
